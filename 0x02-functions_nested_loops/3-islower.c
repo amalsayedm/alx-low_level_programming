bd12d65bf7e880f77ce2c5dfb1b2b3743bdcf400
amalsayedm <amal.sayedm2@gmail.com>
2023-03-21 12:54:20 -0700

    init

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
-/*
- * _islower - "prints"
- */
+/**
+* _islower - prints
+* @c: input param
+* Return: 0 or 1
+*/
 
 int _islower(int c)
 {
-	if (c <= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
