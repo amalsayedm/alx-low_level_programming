b62ddaf70c4346fae95bc784dd5879f84b83d792
amalsayedm <amal.sayedm2@gmail.com>
2023-03-31 15:28:05 -0700

    init

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp, i;
+	int tmp, index;
 
-	for (i = 0; i <= n / 2; i++)
+	for (index = n - 1; index >= n / 2; index--)
 	{
-		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
 	}
 }
