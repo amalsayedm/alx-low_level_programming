839a120b2db46b377de86a8ff74df4c99d105276
amalsayedm <amal.sayedm2@gmail.com>
2023-04-06 00:05:09 +0200

    init

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include<stdio.h>
 
 /**
  * main - check the code
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-    _puts_recursion("Puts with recursion");
+    _print_rev_recursion("\nColton Walker");
     return (0);
 }
