724c2ea342c6c1b6ecea620c6dffd0dfe90ddee0
amalsayedm <amal.sayedm2@gmail.com>
2023-04-05 23:27:49 +0200

    init

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts_recursion - prints astring
+ * @s: the input string
+*/
+
+void _puts_recursion(char *s)
+{
+
+			putchar(*s);
+				if (*s)
+				{
+					s++;
+				_puts_recursion(s);
+				}
+				else
+				{
+					putchar('\n');
+					return;
+				}
+}
