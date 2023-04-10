83eba4474e4b4006490e507ee67e36500d71f0ef
amalsayedm <amal.sayedm2@gmail.com>
2023-04-10 02:49:28 +0200

    init

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: argument count
+ * @argv: input array
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	/* code */
+	printf("%s\n", argv[0]);
+	(void)argc;
+
+	return (0);
+}
