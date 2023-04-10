b44311a39b9e28f42540e9f3f9c469e074317605
amalsayedm <amal.sayedm2@gmail.com>
2023-04-10 03:22:26 +0200

    init

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the mult of 2 numbers
+ * @argc: argument count
+ * @argv: input array
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int res = 0;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (0);
+    }
+
+res = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", res);
+
+	return (0);
+}
