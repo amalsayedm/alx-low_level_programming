0ef1ef5efa1eaacba11029f11dff133d8168a440
amalsayedm <amal.sayedm2@gmail.com>
2023-04-19 00:14:18 +0200

    dfds

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Amal sayed
+ * Desc: Header file
+ */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
