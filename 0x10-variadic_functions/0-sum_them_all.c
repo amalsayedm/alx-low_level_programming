ce74a859e6da577d97d014eed5654d75f309f0aa
amalsayedm <amal.sayedm2@gmail.com>
2023-04-21 00:48:50 +0200

    ds

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,9 +16,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(parms, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(nums, int);
+		sum += va_arg(parms, int);
 
-	va_end(nums);
+	va_end(parms);
 
 	return (sum);
 }
