4d9075802094066945dc1422634309fea3082182
amalsayedm <amal.sayedm2@gmail.com>
2023-04-28 10:57:30 +0200

    ds

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees alist.
+ * @head: A pointer to first node of list.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *helper;
+
+	while (head != null)
+	{
+		helper = head->next;
+		free(head->str);
+		free(head);
+		head = helper;
+	}
+}
