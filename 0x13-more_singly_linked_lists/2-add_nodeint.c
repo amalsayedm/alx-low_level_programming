eb578b67fd00ee5c0579cda0c70dac9c60dcb834
amalsayedm <amal.sayedm2@gmail.com>
2023-04-30 13:02:12 +0200

    kjk

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Adds anode at the beginning of alist
+ * @head: A pointer to the head of the list
+ * @n: integer data
+ * Return: Apointer to the new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	
+	newnode->next = *head;
+newnode->n = n;
+	*head = newnode;
+
+	return (newnode);
+}
